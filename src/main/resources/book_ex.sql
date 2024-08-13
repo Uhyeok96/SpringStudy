1d6c7a29fc4ea03adee68ccd4238f21c4f4a5127
10-5\mit <wlghks2960@naver.com>
2024-08-13 13:07:22 +0900

    v1.0.1 (jdbc 연결 테스트 성공)

---
+++ src/main/resources/book_ex.sql
@@ -0,0 +1,4 @@
+-- 프로젝트에 ojdbc8.jar 연결 한 후에 진행한다.
+-- 프로젝트 우클릭 -> Build Path -> config build path -> Libraries 탭 -> Classpath에 삽입 -> apply
+-- Deployment Assembly -> java build path -> ojdbc8.jar 추가 -> apply
+-- 결과 Referenced Libraries에 ojdbc8.jar 보이면 성공 p80
